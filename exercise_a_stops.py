9803245705852ba4260c7841a706bc4764154f2c
user <user@users-MacBook-Pro.local>
2023-02-28 22:17:24 +0000

    second exercise commit

---
+++ exercise_a_stops.py
@@ -0,0 +1,13 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingstone", "Haymarket" ]
+
+stops.append("Edinburgh Waveverley")
+stops.insert(0,"Glasgow Queen St")
+stops.insert(4,"Polmont")
+print([5])
+del("Livingston")
+stops.pop(2)
+print(stops.__len__)
+sorted(stops)
+stops.reverse()
+for stop in stops:
+    print(stop)
